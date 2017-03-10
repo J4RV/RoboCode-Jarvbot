2e81efe9e433a1cb5779f51a07ad6dee7e6e02cd
Jarv <kira-8@hotmai.com>
2017-03-10 14:36:22 +0100

    Tweaking

--- src/JarvRobot/JarvRobot.java
+++ src/JarvRobot/JarvRobot.java
@@ -39,7 +39,7 @@ public class JarvRobot extends AdvancedRobot {
     public void onScannedRobot(ScannedRobotEvent e) {
         double angle = track(e);
         execute();
-        if (angle <= 3){
+        if (angle <= 2) {
             shoot(e);
             execute();
         }
@@ -63,48 +63,46 @@ public class JarvRobot extends AdvancedRobot {
         angle -= getGunHeading();
 
         setTurnGunRight(angle);
+        setTurnRight(angle);
         turnDegrees = angle;
 
         return angle;
     }
 
     public void shoot(ScannedRobotEvent e) {
-        if (getGunHeat() == 0) {
-            double power = 3;
-            int maxDistance = 800;
+        if (getEnergy() > 5) {
+            if (getGunHeat() == 0) {
+                double power = 3;
+                int maxDistance = 450;
 
-            if (e.getDistance() <= maxDistance) {
-                //Si el objetivo está casi quieto, disparar a máxima potencia
-                if (e.getVelocity() <= 0.5) {
-                    power = 3;
-                } else {
+                if (e.getDistance() <= maxDistance) {
+                    //Si el objetivo está casi quieto, disparar con más potencia
+                    if (e.getVelocity() == 0) {
+                        power = 5;
+                    }
                     //Balas más rápidas a más distancias, balas fuertes y lentas a melee
                     power = power * (1 - e.getDistance() / maxDistance);
                     power = clamp(power, 0.1, 3);
-                }
 
-                fire(power);
+                    fire(power);
+                }
             }
         }
     }
 
-    public void randomMovement(){
-        int maxDistance = 600;
-        int minDistance = 200;
-        double distance = random.nextDouble() * (maxDistance - minDistance) + minDistance;
+    public void randomMovement() {
+        double distance = 100;
 
-        if(runningAhead)
+        if (runningAhead)
             setAhead(distance);
         else
             setBack(distance);
-
-        randomTurn();
     }
 
-    public void randomTurn(){
+    public void randomTurn() {
         //Pequeña posibilidad de cambiar de rotación
-        if(random.nextDouble() >= 0.98){
-            turnDegrees += Math.signum(turnDegrees) * 10;
+        turnDegrees += Math.signum(turnDegrees) * 1;
+        if (random.nextDouble() >= 0.99) {
             turnDegrees *= -1.02;
         }
 
@@ -116,9 +114,9 @@ public class JarvRobot extends AdvancedRobot {
     public static double clamp(double value, double min, double max) {
         double res = value;
 
-        if(value > max){
+        if (value > max) {
             res = max;
-        } else if (value < min){
+        } else if (value < min) {
             res = min;
         }
 
